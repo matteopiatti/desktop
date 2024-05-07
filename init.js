397bc5c1e1d810aa1a7c43cfd63b2adfa7e9a9af
matteopiatti <github@piatti.li>
2024-05-07 13:50:10 +0200

    add Taskbar functionality

--- init.js
+++ init.js
@@ -1,12 +1,20 @@
 import { Box, Draggable, Resizable } from './OS/index.js';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * TODO:
+ * - Remove render function
+ * - Remove unused class functions (can still be added later if needed)
+ * - maybe create a button class?
+ */
 
 class Desktop {
     constructor() {
         this.element = new Box({classList: 'desktop'}).element;
         this.layers = new Box({classList: 'layers'});
-        this.iconLayer = new IconLayer();
         this.windowLayer = new WindowLayer();
-        this.taskBar = new Box({classList: 'task-bar'})
+        this.iconLayer = new IconLayer(this.windowLayer);
+        this.taskBar = new TaskBar(this.windowLayer);
         this.layers.append(this.iconLayer.render())
         this.layers.append(this.windowLayer.render())
         this.element.append(this.layers.render())
@@ -19,9 +27,10 @@ class Desktop {
 }
 
 class IconLayer extends Box {
-    constructor() {
+    constructor(windowLayer) {
         super({classList: 'icon-layer'})
         this.icons = [];
+        this.windowLayer = windowLayer
         this.iconGrid = Array.from({ length: 6 }, () => Array.from({ length: 8 }, () => []))
     }
     
@@ -49,6 +58,7 @@ class WindowLayer extends Box {
         super({classList: 'window-layer'})
         this.windows = []
         this.currentZIndex = 100
+        this.event = new EventTarget()
     }
     
     addWindow(window) {
@@ -57,6 +67,7 @@ class WindowLayer extends Box {
         window.windowLayer = this
         this.currentZIndex++
         this.element.append(window.render())
+        this.event.dispatchEvent(new Event('windowChange'))
     }
     
     getWindow(id) {
@@ -66,6 +77,7 @@ class WindowLayer extends Box {
     deleteWindow(id) {
         this.getWindow(id).delete()
         this.windows = this.windows.filter(window => window.id !== id)
+        this.event.dispatchEvent(new Event('windowChange'))
     }
 }
 
@@ -82,6 +94,14 @@ class Icon extends Draggable {
                 y: Math.floor((this.element.offsetTop + this.element.offsetHeight / 2) / this.element.parentElement.offsetHeight * 6)
             }
         }
+
+        this.element.addEventListener('dblclick', () => {
+            const window = new Window({}, null, text)
+            window.windowLayer = this.iconLayer.windowLayer
+            window.zIndex = this.iconLayer.windowLayer.currentZIndex
+            this.iconLayer.windowLayer.currentZIndex++
+            this.iconLayer.windowLayer.addWindow(window)
+        })
     }
     
     get position() {
@@ -107,9 +127,11 @@ class Icon extends Draggable {
 class Window extends Resizable {
     constructor(options, children, title, content, size, position) {
         super(options, children, null);
+        this.id = uuidv4();
         this.element.classList.add('window');
         this.windowHeader = new Box({classList: 'window-header'});
         this.windowContent = new Box({classList: 'window-content'});
+        this.windowContent.append(content);
         this.element.append(this.windowHeader.render());
         this.element.append(this.windowContent.render());
         this.windowContent.element.addEventListener('mousedown', (e) => {
@@ -120,6 +142,13 @@ class Window extends Resizable {
         this.title = title;
         this.initWindowHeader();
         this.maximized = false;
+        this.minimized = false;
+
+        // TODO: fix this later
+        this.initialWidth = this.element.offsetWidth;
+        this.initialHeight = this.element.offsetHeight;
+        this.initialTop = this.element.offsetTop;
+        this.initialLeft = this.element.offsetLeft;
 
         super.callback = () => {
             this.zIndex = this.windowLayer.currentZIndex;
@@ -137,7 +166,6 @@ class Window extends Resizable {
     }
 
     initWindowHeader() {
-        this.windowHeader.element.innerHTML = '';
         this.windowHeader.element.append(this.title);
         const windowButtons = new Box({classList: 'window-buttons'});
         
@@ -147,38 +175,86 @@ class Window extends Resizable {
         windowButtons.append(minimizeButton.render());
         windowButtons.append(maximizeButton.render());
         windowButtons.append(closeButton.render());
-        this.windowHeader.append(windowButtons.render());
-        minimizeButton.element.addEventListener('click', () => this.minimize());
+        minimizeButton.element.addEventListener('click', () => this.minimize(this.minimized));
         maximizeButton.element.addEventListener('click', () => this.maximize());
-        closeButton.element.addEventListener('click', () => this.delete());
+        closeButton.element.addEventListener('click', () => {
+            this.delete()
+            this.windowLayer.deleteWindow(this.id)
+        });
 
         windowButtons.element.addEventListener('mousedown', (e) => {
             e.stopPropagation();
             this.zIndex = this.windowLayer.currentZIndex;
             this.windowLayer.currentZIndex++;
         })
+        this.windowHeader.append(windowButtons.render());
     }
 
-    minimize() {     
-        this.element.style.display = 'none';
+    minimize(minimized) {
+        this.element.style.display = minimized ? '' : 'none';
+        this.minimized = !minimized;
+        this.windowLayer.event.dispatchEvent(new Event('windowChange'));
     }
 
     maximize() {
-        if (this.maximized) {
-            this.maximized = false;
-            this.element.style.width = this.initialWidth + 'px';
-            this.element.style.height = this.initialHeight + 'px';
-            this.element.style.left = this.initialLeft + 'px';
-            this.element.style.top = this.initialTop + 'px';
-        } else {
-            this.maximized = true;
-            this.element.style.width = '100%';
-            this.element.style.height = '100%';
-            this.element.style.left = '0';
-            this.element.style.top = '0';
-        }
+        this.element.style.width = this.maximized ? this.initialWidth + 'px' : '100%';
+        this.element.style.height = this.maximized ? this.initialHeight + 'px' : '100%';
+        this.element.style.left = this.maximized ? this.initialLeft + 'px' : '0';
+        this.element.style.top = this.maximized ? this.initialTop + 'px' : '0';
+        this.maximized = !this.maximized;
     }
 }
+
+class TaskBar extends Box {
+    constructor(windowLayer) {
+        super({classList: 'task-bar'});
+        this.windowLayer = windowLayer
+        this.startButton = new Box({classList: 'start-button button', role: 'button'}, 'Start');
+        this.startButton.element.addEventListener('click', () => this.toggleStartMenu());
+        this.startMenu = this.initStartMenu();
+        this.windows = new Box({classList: 'task-bar-windows'});
+        this.append(this.startButton.render());
+        this.append(this.windows.render());
+
+        this.windowLayer.event.addEventListener('windowChange', () => this.updateWindows())
+        document.addEventListener('mousedown', (e) => {
+            if (!this.startButton.element.contains(e.target) && !this.startMenu.element.contains(e.target) && this.startMenu.element.style.display !== 'none') {
+                this.toggleStartMenu()
+            }
+        })
+    }
+
+    toggleStartMenu() {
+        this.startMenu.element.style.display = this.startMenu.element.style.display === 'none' ? 'flex' : 'none';    
+    }
+
+    initStartMenu() {
+        const startMenu = new Box({classList: 'start-menu'});
+        this.append(startMenu.render());
+        const startMenuItems = new Box({classList: 'start-menu-items'});
+        startMenu.append(startMenuItems.render());
+        startMenu.element.style.display = 'none';
+        startMenuItems.append(new Box({classList: 'start-menu-item'}, 'Item 1').render());
+        return startMenu;
+    }
+
+    updateWindows() {
+        this.windows.element.innerHTML = '';
+        this.windowLayer.windows.forEach(window => {
+            let classes = 'task-button button';
+            if (window.minimized) classes += ' minimized';
+            const taskButton = new Box({classList: classes}, window.title);
+            taskButton.element.addEventListener('click', () => {
+                window.zIndex = this.windowLayer.currentZIndex;
+                this.windowLayer.currentZIndex++;
+                window.minimize(true);
+            });
+            this.windows.append(taskButton.render());
+        });
+    }
+}
+
+
 
 const desktop = new Desktop();
 desktop.iconLayer.addIcon(new Icon('icon1', 'Icon 1', {x:2,y:1}))
